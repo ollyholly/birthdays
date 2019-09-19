a1176a925af5f05b41637aa5e1fe222051405d8c
Olly Holovchenko <oleh.holo@gmail.com>
2019-09-19 22:26:51 +0100

    Add tests and features for adding and showing friends birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,15 @@
+class BirthdayList
+  def initialize
+    @list = []
+  end
 
+  def add(name, birthday)
+    @list << { name: name, birthday: birthday }
+  end
+
+  def show
+    @list.each do |friend| 
+      puts "#{friend[:name]}, #{friend[:birthday]}" 
+    end
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,24 @@
+require 'birthday_list'
+require 'timecop'
 
+describe BirthdayList do
+
+  describe '#store_birthday' do
+    it { is_expected.to respond_to(:add).with(2).arguments }
+
+    it 'adds friend\'s birthday' do
+      birthday_list = BirthdayList.new
+
+      expect(birthday_list.add("Bob", "13 April 1985")).to eq [{ name: "Bob", birthday: "13 April 1985" }]
+    end
+
+    it 'shows friends birthdays' do
+      birthday_list = BirthdayList.new
+      birthday_list.add("Bob", "13 April 1985")
+      birthday_list.add("Amber", "20 May 1987")
+
+      expect{birthday_list.show}.to output("Bob, 13 April 1985\nAmber, 20 May 1987\n").to_stdout
+    end
+  end
+
+end
